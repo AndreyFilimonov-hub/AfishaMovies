b61f8895aeffad744cc0480fb3e0ac2b022986e5
Andrey Filimonov <andrfil05@mail.ru>
2025-09-15 10:34:00 +0600

    create entities

---
+++ app/src/main/java/com/filimonov/afishamovies/domain/entities/PersonBannerEntity.kt
@@ -0,0 +1,8 @@
+package com.filimonov.afishamovies.domain.entities
+
+data class PersonBannerEntity(
+    val id: Int,
+    val name: String,
+    val character: String?,
+    val profession: String
+)
