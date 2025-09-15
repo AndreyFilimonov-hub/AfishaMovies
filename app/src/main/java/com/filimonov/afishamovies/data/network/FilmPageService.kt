9f7089f2cd6bde8e4100bdd88bc51a234b86217a
Andrey Filimonov <andrfil05@mail.ru>
2025-09-15 17:42:27 +0600

    add method and network request

--- app/src/main/java/com/filimonov/afishamovies/data/network/FilmPageService.kt
+++ app/src/main/java/com/filimonov/afishamovies/data/network/FilmPageService.kt
@@ -2,9 +2,11 @@ package com.filimonov.afishamovies.data.network
 
 import com.filimonov.afishamovies.BuildConfig
 import com.filimonov.afishamovies.data.model.filmpage.FilmPageDto
+import com.filimonov.afishamovies.data.model.filmpage.ImagePreviewResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface FilmPageService {
 
@@ -13,6 +15,16 @@ interface FilmPageService {
     suspend fun getFilmPageById(
         @Path("id") id: Int
     ) : FilmPageDto
+
+    @Headers("X-API-KEY:$API_KEY")
+    @GET("image")
+    suspend fun getPreviewImagesByMovieId(
+        @Query("movieId") movieId: Int,
+        @Query("page") page: Int = 1,
+        @Query("limit") limit: Int = 10,
+        @Query("selectedFields") selectedFieldId: String = "movieId",
+        @Query("selectedFields") selectedFieldName: String = "url",
+    ) : ImagePreviewResponse
 
     companion object {
 
--- app/src/main/java/com/filimonov/afishamovies/data/repository/FilmPageRepositoryImpl.kt
+++ app/src/main/java/com/filimonov/afishamovies/data/repository/FilmPageRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.filimonov.afishamovies.data.repository
 
 import com.filimonov.afishamovies.data.mapper.toEntity
+import com.filimonov.afishamovies.data.mapper.toImagePreviewListEntity
 import com.filimonov.afishamovies.data.network.FilmPageService
 import com.filimonov.afishamovies.domain.entities.FilmPageEntity
+import com.filimonov.afishamovies.domain.entities.ImagePreviewEntity
 import com.filimonov.afishamovies.domain.repository.FilmPageRepository
 import javax.inject.Inject
 
@@ -11,5 +13,9 @@ class FilmPageRepositoryImpl @Inject constructor(
 ) : FilmPageRepository {
     override suspend fun getFilmPageById(id: Int): FilmPageEntity {
         return apiService.getFilmPageById(id).toEntity()
+    }
+
+    override suspend fun getImagePreviewsByMovieId(movieId: Int): List<ImagePreviewEntity> {
+        return apiService.getPreviewImagesByMovieId(movieId = movieId).images.toImagePreviewListEntity()
     }
 }
