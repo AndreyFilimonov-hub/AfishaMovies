b282ccebeb3763fba040ebcec4a92066eedcb763
Andrey Filimonov <andrfil05@mail.ru>
2025-09-16 20:47:06 +0600

    add person adapter

---
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/filmpage/personadapter/PersonViewHolder.kt
@@ -0,0 +1,36 @@
+package com.filimonov.afishamovies.presentation.ui.filmpage.personadapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.filimonov.afishamovies.databinding.ItemPersonBinding
+import com.filimonov.afishamovies.domain.entities.PersonBannerEntity
+
+class PersonViewHolder(
+    private val binding: ItemPersonBinding
+) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    companion object {
+
+        private const val RADIUS_PX = 4
+    }
+
+    fun bind(person: PersonBannerEntity) {
+
+        binding.tvName.text = person.name
+        if (person.profession == "актеры") {
+            binding.tvCharacter.text = person.character
+        } else {
+            binding.tvCharacter.text = person.profession.substring(0, person.profession.lastIndex)
+        }
+        Glide.with(binding.ivAvatar)
+            .load(person.photo)
+            .transform(
+                CenterCrop(),
+                RoundedCorners((RADIUS_PX * binding.root.context.resources.displayMetrics.density).toInt())
+            )
+            .into(binding.ivAvatar)
+    }
+}
