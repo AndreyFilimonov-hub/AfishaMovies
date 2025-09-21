b4b59db114889ec2b44b4dfafaf13e19323b01be
Andrey Filimonov <andrfil05@mail.ru>
2025-09-21 11:10:32 +0600

    add ListPageState models for actor and workers and create viewholders

--- app/src/main/java/com/filimonov/afishamovies/presentation/ui/listpage/mediabannergridadapter/ListPageMedia.kt
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/listpage/mediabannergridadapter/ListPageMedia.kt
@@ -1,11 +1,20 @@
 package com.filimonov.afishamovies.presentation.ui.listpage.mediabannergridadapter
 
 import com.filimonov.afishamovies.domain.entities.MediaBannerEntity
+import com.filimonov.afishamovies.domain.entities.PersonBannerEntity
 
 sealed class ListPageMedia {
 
-    data class Banner(
-        val media: MediaBannerEntity
+    data class MediaBanner(
+        val media: MediaBannerEntity,
+    ) : ListPageMedia()
+
+    data class ActorBanner(
+        val actor: PersonBannerEntity,
+    ) : ListPageMedia()
+
+    data class WorkerBanner(
+        val worker: PersonBannerEntity,
     ) : ListPageMedia()
 
     data object Loading : ListPageMedia()
--- app/src/main/java/com/filimonov/afishamovies/presentation/ui/listpage/mediabannergridadapter/MediaBannerGridAdapter.kt
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/listpage/mediabannergridadapter/MediaBannerGridAdapter.kt
@@ -8,27 +8,49 @@ import com.filimonov.afishamovies.databinding.ItemBannerBinding
 import com.filimonov.afishamovies.databinding.ItemErrorLoadingBinding
 import com.filimonov.afishamovies.databinding.ItemLoadingProgressBinding
 import com.filimonov.afishamovies.domain.entities.MediaBannerEntity
+import com.filimonov.afishamovies.domain.entities.PersonBannerEntity
 
 class MediaBannerGridAdapter(
     private val onMediaBannerClick: (MediaBannerEntity) -> Unit,
+    private val onPersonBannerClick: (PersonBannerEntity) -> Unit,
     private val onRetryButtonClick: () -> Unit,
 ) : ListAdapter<ListPageMedia, RecyclerView.ViewHolder>(ListPageMediaDiffCallback()) {
 
     companion object {
-        const val BANNER_TYPE = 0
-        const val LOADING_TYPE = 1
-        const val ERROR_TYPE = 2
+        const val MEDIA_BANNER_TYPE = 0
+        const val ACTOR_BANNER_TYPE = 1
+        const val WORKER_BANNER_TYPE = 2
+        const val LOADING_TYPE = 3
+        const val ERROR_TYPE = 4
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
-            BANNER_TYPE -> ListPageMediaBannerViewHolder(
+            MEDIA_BANNER_TYPE -> ListPageMediaBannerViewHolder(
                 ItemBannerBinding.inflate(
                     LayoutInflater.from(parent.context),
                     parent,
                     false
                 ),
                 onMediaBannerClick
+            )
+
+            ACTOR_BANNER_TYPE -> ListPageActorBannerViewHolder(
+                ItemBannerBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                ),
+                onPersonBannerClick
+            )
+
+            WORKER_BANNER_TYPE -> ListPageWorkerBannerViewHolder(
+                ItemBannerBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                ),
+                onPersonBannerClick
             )
 
             LOADING_TYPE -> ListPageLoadingViewHolder(
@@ -52,7 +74,9 @@ class MediaBannerGridAdapter(
 
     override fun getItemViewType(position: Int): Int {
         return when (getItem(position)) {
-            is ListPageMedia.Banner -> BANNER_TYPE
+            is ListPageMedia.MediaBanner -> MEDIA_BANNER_TYPE
+            is ListPageMedia.ActorBanner -> ACTOR_BANNER_TYPE
+            is ListPageMedia.WorkerBanner -> WORKER_BANNER_TYPE
             ListPageMedia.Loading -> LOADING_TYPE
             else -> ERROR_TYPE
         }
@@ -61,7 +85,9 @@ class MediaBannerGridAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
         when (val item = getItem(position)) {
-            is ListPageMedia.Banner -> (holder as ListPageMediaBannerViewHolder).bind(item)
+            is ListPageMedia.MediaBanner -> (holder as ListPageMediaBannerViewHolder).bind(item)
+            is ListPageMedia.ActorBanner -> (holder as ListPageActorBannerViewHolder).bind(item)
+            is ListPageMedia.WorkerBanner -> (holder as ListPageWorkerBannerViewHolder).bind(item)
             is ListPageMedia.Error -> (holder as ListPageErrorViewHolder).bind()
             else -> {}
         }
