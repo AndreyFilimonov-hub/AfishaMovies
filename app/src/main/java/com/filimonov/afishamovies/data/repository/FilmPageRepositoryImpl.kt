18fec7ec1b94e29881dcd0448cbba3b257eaf5a6
Andrey Filimonov <andrfil05@mail.ru>
2025-09-21 11:09:00 +0600

    add usecases

--- app/src/main/java/com/filimonov/afishamovies/data/repository/FilmPageRepositoryImpl.kt
+++ app/src/main/java/com/filimonov/afishamovies/data/repository/FilmPageRepositoryImpl.kt
@@ -5,17 +5,29 @@ import com.filimonov.afishamovies.data.mapper.toImagePreviewListEntity
 import com.filimonov.afishamovies.data.network.FilmPageService
 import com.filimonov.afishamovies.domain.entities.FilmPageEntity
 import com.filimonov.afishamovies.domain.entities.ImagePreviewEntity
+import com.filimonov.afishamovies.domain.entities.PersonBannerEntity
 import com.filimonov.afishamovies.domain.repository.FilmPageRepository
 import javax.inject.Inject
 
 class FilmPageRepositoryImpl @Inject constructor(
     private val apiService: FilmPageService
 ) : FilmPageRepository {
+
+    private val persons = mutableMapOf<Int, List<PersonBannerEntity>>()
+
     override suspend fun getFilmPageById(id: Int): FilmPageEntity {
-        return apiService.getFilmPageById(id).toFilmPageEntity()
+        return apiService.getFilmPageById(id).toFilmPageEntity().also { persons[id] = it.persons }
     }
 
     override suspend fun getImagePreviewsByMovieId(movieId: Int): List<ImagePreviewEntity> {
         return apiService.getPreviewImagesByMovieId(movieId = movieId).images.toImagePreviewListEntity()
+    }
+
+    override fun getPersonList(id: Int): List<PersonBannerEntity> {
+        return persons[id] ?: emptyList()
+    }
+
+    override fun clearCachedPersonList(movieId: Int) {
+        persons.remove(movieId)
     }
 }
