88f93c2e43f743b9b2dec58ad87bbd65d2b91b48
Andrey Filimonov <andrfil05@mail.ru>
2025-10-13 16:36:25 +0600

    setup searchbar

--- app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/SearchPageFragment.kt
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/SearchPageFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.filimonov.afishamovies.databinding.FragmentSearchPageBinding
 
@@ -24,6 +25,17 @@ class SearchPageFragment : Fragment() {
     ): View {
         _binding = FragmentSearchPageBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSearchBar()
+    }
+
+    private fun setupSearchBar() {
+        binding.ivFilter.setOnClickListener {
+            Toast.makeText(requireContext(), "filter", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
