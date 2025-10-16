3e795a7836747a0575ea59bd36c5db56fdb0b532
Andrey Filimonov <andrfil05@mail.ru>
2025-10-16 22:49:11 +0600

    create SearchPageFragment.kt and view

---
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/SearchSettingsFragment.kt
@@ -0,0 +1,100 @@
+package com.filimonov.afishamovies.presentation.ui.searchpage
+
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.filimonov.afishamovies.R
+import com.filimonov.afishamovies.databinding.FragmentSearchSettingsBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SearchSettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SearchSettingsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentSearchSettingsBinding? = null
+
+    private val binding: FragmentSearchSettingsBinding
+        get() = _binding ?: throw kotlin.RuntimeException("FragmentSearchSettingsBinding == null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchSettingsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRangeSlider()
+    }
+
+    private fun setupRangeSlider() {
+        with(binding.rangeSlider) {
+            val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.thumb_circle)
+                ?: throw RuntimeException()
+            thumbRadius = (resources.displayMetrics.density * 12).toInt()
+            setCustomThumbDrawable(drawable)
+            trackActiveTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.blue))
+            trackInactiveTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    requireContext(),
+                    android.R.color.darker_gray
+                )
+            )
+            addOnChangeListener { slider, _, _ ->
+                val min = slider.values[0].toInt()
+                val max = slider.values[1].toInt()
+                val rating = if (min == max) {
+                    min.toString()
+                } else {
+                    "$min - $max"
+                }
+                binding.tvRating.text = rating
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SearchSettingsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SearchSettingsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
