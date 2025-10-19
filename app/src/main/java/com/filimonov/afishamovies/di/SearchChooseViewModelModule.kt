989e3892d1d0325abfbc408653ae7e188260a1b5
Andrey Filimonov <andrfil05@mail.ru>
2025-10-19 17:04:54 +0600

    create SearchChooseComponent

---
+++ app/src/main/java/com/filimonov/afishamovies/di/SearchChooseViewModelModule.kt
@@ -0,0 +1,16 @@
+package com.filimonov.afishamovies.di
+
+import androidx.lifecycle.ViewModel
+import com.filimonov.afishamovies.presentation.ui.searchpage.searchchoosefragment.SearchChooseViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+interface SearchChooseViewModelModule {
+
+    @IntoMap
+    @ViewModelKey(SearchChooseViewModel::class)
+    @Binds
+    fun bindListPageViewModel(viewModel: SearchChooseViewModel): ViewModel
+}
