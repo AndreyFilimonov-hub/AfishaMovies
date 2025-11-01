fd0a5603b702457edbc40aa0bbecd2c6c613f40f
Andrey Filimonov <andrfil05@mail.ru>
2025-11-01 23:30:29 +0600

    create enums

---
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/searchsettingsfragment/searchchoosefragment/Genres.kt
@@ -0,0 +1,12 @@
+package com.filimonov.afishamovies.presentation.ui.searchpage.searchsettingsfragment.searchchoosefragment
+
+import com.filimonov.afishamovies.R
+
+enum class Genres(override val itemResId: Int) : ChooseItem {
+
+    COMEDY(R.string.comedy),
+    MELODRAMA(R.string.melodrama),
+    ACTION(R.string.action),
+    WESTERN(R.string.western),
+    DRAMA(R.string.drama)
+}
