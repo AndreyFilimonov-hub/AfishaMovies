180bb9ae46db6b5704e84c93cad5eab73251a3f8
Andrey Filimonov <andrfil05@mail.ru>
2025-11-05 20:43:45 +0600

    add any to enums

--- app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/searchsettingsfragment/searchchoosefragment/Countries.kt
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/searchsettingsfragment/searchchoosefragment/Countries.kt
@@ -4,6 +4,7 @@ import com.filimonov.afishamovies.R
 
 enum class Countries(override val itemResId: Int) : ChooseItem {
 
+    ANY(R.string.any_v2),
     RUSSIA(R.string.russia),
     GB(R.string.great_britain),
     GERMANY(R.string.germany),
--- app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/searchsettingsfragment/searchchoosefragment/Genres.kt
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/searchsettingsfragment/searchchoosefragment/Genres.kt
@@ -4,6 +4,7 @@ import com.filimonov.afishamovies.R
 
 enum class Genres(override val itemResId: Int) : ChooseItem {
 
+    ANY(R.string.any),
     COMEDY(R.string.comedy),
     MELODRAMA(R.string.melodrama),
     ACTION(R.string.action),
