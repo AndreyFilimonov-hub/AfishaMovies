c41a5de1d235755864f50f1bb45afc83d0f10d5f
Andrey Filimonov <andrfil05@mail.ru>
2025-11-08 20:02:46 +0600

    remove persons from search

--- app/src/main/java/com/filimonov/afishamovies/data/repository/SearchPageRepositoryImpl.kt
+++ app/src/main/java/com/filimonov/afishamovies/data/repository/SearchPageRepositoryImpl.kt
@@ -1,9 +1,7 @@
 package com.filimonov.afishamovies.data.repository
 
 import com.filimonov.afishamovies.data.mapper.toSearchMediaBannerListEntity
-import com.filimonov.afishamovies.data.mapper.toSearchPersonBannerListEntity
 import com.filimonov.afishamovies.data.network.SearchPageService
-import com.filimonov.afishamovies.domain.entities.PersonBannerEntity
 import com.filimonov.afishamovies.domain.entities.SearchMediaBannerEntity
 import com.filimonov.afishamovies.domain.repository.SearchPageRepository
 import javax.inject.Inject
@@ -12,9 +10,5 @@ class SearchPageRepositoryImpl @Inject constructor(private val apiService: Searc
 
     override suspend fun getMoviesByQuery(page: Int, query: String): List<SearchMediaBannerEntity> {
         return apiService.getMoviesByQuery(page = page, query = query).medias.toSearchMediaBannerListEntity()
-    }
-
-    override suspend fun getPersonsByQuery(page: Int, query: String): List<PersonBannerEntity> {
-        return apiService.getPersonsByQuery(page = page, query = query).persons.toSearchPersonBannerListEntity()
     }
 }
