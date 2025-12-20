a4caf406bbb29879794001c4c701cb0df825ca39
Andrey Filimonov <andrfil05@mail.ru>
2025-12-20 20:16:09 +0600

    create usecases

---
+++ app/src/main/java/com/filimonov/afishamovies/domain/usecases/GetWasInterestingMediaBanners.kt
@@ -0,0 +1,12 @@
+package com.filimonov.afishamovies.domain.usecases
+
+import com.filimonov.afishamovies.domain.entities.MediaBannerEntity
+import com.filimonov.afishamovies.domain.repository.ProfilePageRepository
+import javax.inject.Inject
+
+class GetWasInterestingMediaBanners @Inject constructor(private val repository: ProfilePageRepository) {
+
+    operator fun invoke(): List<MediaBannerEntity> {
+        return repository.getWasInterestingMediaBannerList()
+    }
+}
