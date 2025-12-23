d85cb61bb5c2e76592700d4f60e4ba7f5168aa82
Andrey Filimonov <andrfil05@mail.ru>
2025-12-23 16:34:37 +0600

    replace context to Application

--- app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/searchsettingsfragment/searchchoosefragment/SearchChooseViewModel.kt
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/searchpage/searchsettingsfragment/searchchoosefragment/SearchChooseViewModel.kt
@@ -1,9 +1,9 @@
 package com.filimonov.afishamovies.presentation.ui.searchpage.searchsettingsfragment.searchchoosefragment
 
-import android.content.Context
+import android.app.Application
 import androidx.lifecycle.ViewModel
-import com.filimonov.afishamovies.di.searchpagecomponent.searchsettingscomponent.searchchoosecomponent.ChooseItemQualifier
 import com.filimonov.afishamovies.di.ModeQualifier
+import com.filimonov.afishamovies.di.searchpagecomponent.searchsettingscomponent.searchchoosecomponent.ChooseItemQualifier
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
@@ -11,7 +11,7 @@ import javax.inject.Inject
 class SearchChooseViewModel @Inject constructor(
     @ModeQualifier private val mode: FilterMode,
     @ChooseItemQualifier var chooseItem: String?,
-    private val applicationContext: Context
+    private val appContext: Application
 ) : ViewModel() {
 
     private val list: List<ChooseItem> = if (mode == FilterMode.GENRE) {
@@ -24,7 +24,7 @@ class SearchChooseViewModel @Inject constructor(
     val state = _state.asStateFlow()
 
     fun sendRequest(query: String) {
-        val filteredList = list.filter { applicationContext.getString(it.itemResId).contains(query.lowercase()) }
+        val filteredList = list.filter { appContext.getString(it.itemResId).contains(query.lowercase()) }
         _state.value = SearchChooseState.Search(filteredList)
     }
 }
