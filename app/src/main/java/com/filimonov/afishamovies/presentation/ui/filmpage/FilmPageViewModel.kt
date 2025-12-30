5cd92f43d3e6a516be3ca2adea32c6668fae3f82
Andrey Filimonov <andrfil05@mail.ru>
2025-12-30 14:34:44 +0600

    create methods to save and delete from db

--- app/src/main/java/com/filimonov/afishamovies/presentation/ui/filmpage/FilmPageViewModel.kt
+++ app/src/main/java/com/filimonov/afishamovies/presentation/ui/filmpage/FilmPageViewModel.kt
@@ -7,13 +7,17 @@ import com.filimonov.afishamovies.domain.entities.FilmPageEntity
 import com.filimonov.afishamovies.domain.entities.MediaBannerEntity
 import com.filimonov.afishamovies.domain.entities.PersonBannerEntity
 import com.filimonov.afishamovies.domain.enums.DefaultCollection
+import com.filimonov.afishamovies.domain.usecases.AddFilmPageToDbUseCase
 import com.filimonov.afishamovies.domain.usecases.AddMediaBannerToCollectionUseCase
 import com.filimonov.afishamovies.domain.usecases.AddMediaBannerToInterestedCollectionUseCase
-import com.filimonov.afishamovies.domain.usecases.ClearCachedPersonListUseCase
+import com.filimonov.afishamovies.domain.usecases.ClearCachedPersonListAndSimilarMoviesUseCase
+import com.filimonov.afishamovies.domain.usecases.DeleteMediaBannerFromCollectionUseCase
+import com.filimonov.afishamovies.domain.usecases.DeleteUnusedFilmPageUseCase
 import com.filimonov.afishamovies.domain.usecases.GetCollectionIdByKeyUseCase
 import com.filimonov.afishamovies.domain.usecases.GetFilmPageByIdUseCase
 import com.filimonov.afishamovies.domain.usecases.GetImagePreviewListByMovieIdUseCase
 import com.filimonov.afishamovies.domain.usecases.GetMediaBannerByIdFromLocalUseCase
+import com.filimonov.afishamovies.domain.usecases.UpdateDefaultCategoriesFlagsUseCase
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -30,9 +34,12 @@ class FilmPageViewModel @Inject constructor(
     private val getMediaBannerByIdFromLocalUseCase: GetMediaBannerByIdFromLocalUseCase,
     private val getFilmPageByIdUseCase: GetFilmPageByIdUseCase,
     private val getImagePreviewListByMovieIdUseCase: GetImagePreviewListByMovieIdUseCase,
-    private val clearCachedPersonListUseCase: ClearCachedPersonListUseCase
+    private val clearCachedPersonListAndSimilarMoviesUseCase: ClearCachedPersonListAndSimilarMoviesUseCase,
+    private val addFilmPageToDbUseCase: AddFilmPageToDbUseCase,
+    private val deleteMediaBannerFromCollectionUseCase: DeleteMediaBannerFromCollectionUseCase,
+    private val deleteUnusedFilmPageUseCase: DeleteUnusedFilmPageUseCase,
+    private val updateDefaultCategoriesFlagsUseCase: UpdateDefaultCategoriesFlagsUseCase
 ) : ViewModel() {
-
 
     private val _state: MutableStateFlow<FilmPageState> = MutableStateFlow(FilmPageState.Loading)
 
@@ -47,15 +54,28 @@ class FilmPageViewModel @Inject constructor(
             try {
                 coroutineScope {
                     val filmPage = async { getFilmPageByIdUseCase(movieId) }
-                    val imagePreviews = async { getImagePreviewListByMovieIdUseCase(movieId) }
+                    val imagePreviews = async {
+                        try {
+                            getImagePreviewListByMovieIdUseCase(movieId)
+                        } catch (_: Exception) {
+                            null
+                        }
+                    }
 
-                    _state.value = FilmPageState.Success(filmPage.await(), imagePreviews.await().take(10))
+                    _state.value =
+                        FilmPageState.Success(filmPage.await(), imagePreviews.await()?.take(10))
                 }
             } catch (e: CancellationException) {
                 throw e
             } catch (_: Exception) {
                 _state.value = FilmPageState.Error
             }
+        }
+    }
+
+    private fun saveFilmPageToDb(filmPageEntity: FilmPageEntity) {
+        viewModelScope.launch {
+            addFilmPageToDbUseCase(filmPageEntity)
         }
     }
 
@@ -92,7 +112,7 @@ class FilmPageViewModel @Inject constructor(
             .toString()
     }
 
-    fun addMediaToDefaultCollection(filmPageEntity: FilmPageEntity, collection: DefaultCollection) {
+    private fun addMediaBannerToDefaultCollection(filmPageEntity: FilmPageEntity, collection: DefaultCollection) {
         viewModelScope.launch {
             val collectionId = getCollectionIdByKeyUseCase(collection.key)
             val mediaBannerEntity = getMediaBannerByIdFromLocalUseCase(filmPageEntity.id)
@@ -106,8 +126,78 @@ class FilmPageViewModel @Inject constructor(
         }
     }
 
+    private fun deleteFromCollection(
+        filmId: Int,
+        collectionId: Int? = null,
+        collection: DefaultCollection
+    ) {
+        viewModelScope.launch {
+            if (collectionId == null) {
+                val collectionIdFromDb = getCollectionIdByKeyUseCase(collection.key)
+                deleteMediaBannerFromCollectionUseCase(filmId, collectionIdFromDb)
+            } else {
+                deleteMediaBannerFromCollectionUseCase(filmId, collectionId)
+            }
+            deleteUnusedFilmPageUseCase()
+        }
+    }
+
+    fun toggleLike() {
+        val currentState = _state.value
+        if (currentState !is FilmPageState.Success) return
+
+        val updatedEntity = currentState.filmPage.copy(isLiked = !currentState.filmPage.isLiked)
+        _state.value = currentState.copy(filmPage = updatedEntity)
+
+        viewModelScope.launch {
+            if (updatedEntity.isLiked) {
+                saveFilmPageToDb(updatedEntity)
+                addMediaBannerToDefaultCollection(updatedEntity, DefaultCollection.LIKED)
+            } else {
+                deleteFromCollection(filmId =  updatedEntity.id, collection =  DefaultCollection.LIKED)
+            }
+            updateDefaultCategoriesFlagsUseCase(filmId = updatedEntity.id, isLiked = updatedEntity.isLiked)
+        }
+    }
+
+    fun toggleWantToWatch() {
+        val currentState = _state.value
+        if (currentState !is FilmPageState.Success) return
+
+        val updatedEntity = currentState.filmPage.copy(isWantToWatch = !currentState.filmPage.isWantToWatch)
+        _state.value = currentState.copy(filmPage = updatedEntity)
+
+        viewModelScope.launch {
+            if (updatedEntity.isWantToWatch) {
+                saveFilmPageToDb(updatedEntity)
+                addMediaBannerToDefaultCollection(updatedEntity, DefaultCollection.WANT_TO_WATCH)
+            } else {
+                deleteFromCollection(filmId =  updatedEntity.id, collection =  DefaultCollection.WANT_TO_WATCH)
+            }
+            updateDefaultCategoriesFlagsUseCase(filmId = updatedEntity.id, isWantToWatch = updatedEntity.isWantToWatch)
+        }
+    }
+
+    fun toggleWatched() {
+        val currentState = _state.value
+        if (currentState !is FilmPageState.Success) return
+
+        val updatedEntity = currentState.filmPage.copy(isWatched = !currentState.filmPage.isWatched)
+        _state.value = currentState.copy(filmPage = updatedEntity)
+
+        viewModelScope.launch {
+            if (updatedEntity.isWatched) {
+                saveFilmPageToDb(updatedEntity)
+                addMediaBannerToDefaultCollection(updatedEntity, DefaultCollection.WATCHED)
+            } else {
+                deleteFromCollection(filmId =  updatedEntity.id, collection =  DefaultCollection.WATCHED)
+            }
+            updateDefaultCategoriesFlagsUseCase(filmId = updatedEntity.id, isWatched = updatedEntity.isWatched)
+        }
+    }
+
     override fun onCleared() {
         super.onCleared()
-        clearCachedPersonListUseCase(movieId)
+        clearCachedPersonListAndSimilarMoviesUseCase(movieId)
     }
 }
