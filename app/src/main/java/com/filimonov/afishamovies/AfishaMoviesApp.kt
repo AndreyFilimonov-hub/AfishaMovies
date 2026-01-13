bf4c2c393aeeca530856a09e2da2802a3cd44fe0
Andrey Filimonov <andrfil05@mail.ru>
2026-01-13 16:02:14 +0600

    start worker

--- app/src/main/java/com/filimonov/afishamovies/AfishaMoviesApp.kt
+++ app/src/main/java/com/filimonov/afishamovies/AfishaMoviesApp.kt
@@ -1,27 +1,42 @@
 package com.filimonov.afishamovies
 
 import android.app.Application
-import com.filimonov.afishamovies.data.database.AfishaDataBase
+import androidx.work.Configuration
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
+import com.filimonov.afishamovies.data.workers.ClearUnusedDataWorker
+import com.filimonov.afishamovies.data.workers.ClearUnusedDataWorkerFactory
 import com.filimonov.afishamovies.di.AppComponent
 import com.filimonov.afishamovies.di.DaggerAppComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AfishaMoviesApp : Application() {
+class AfishaMoviesApp : Application(), Configuration.Provider {
+
     val component: AppComponent by lazy {
         DaggerAppComponent.factory()
             .create(this)
     }
 
+    @Inject
+    lateinit var clearUnusedDataWorkerFactory: ClearUnusedDataWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
-        // TODO maybe rewrite with service
-        val db = AfishaDataBase.getInstance(this)
-        CoroutineScope(Dispatchers.IO).launch {
-            db.collectionMediaBannerDao().clearInterestedCollection()
-            db.mediaBannerDao().deleteUnusedMediaBanners()
-            db.filmPageDao().deleteUnusedFilmPages()
-        }
+        component.inject(this)
+        startClearUnusedDataWorker()
+    }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(clearUnusedDataWorkerFactory)
+            .build()
+
+    private fun startClearUnusedDataWorker() {
+        val workerManager = WorkManager.getInstance(this)
+        workerManager.enqueueUniqueWork(
+            ClearUnusedDataWorker.NAME,
+            ExistingWorkPolicy.KEEP,
+            ClearUnusedDataWorker.makeRequest()
+        )
     }
 }
